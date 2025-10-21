289c1030cfbd9444133493a99f7d305fb7daf00c
miguel <moalmiguel@gmail.com>
2025-10-21 10:25:51 -0600

    Refactor type declarations for ImportMetaEnv and ImportMeta into initSupabase.ts; remove vite-env.d.ts file.

--- src/supabase/initSupabase.ts
+++ src/supabase/initSupabase.ts
@@ -2,6 +2,18 @@ import { createClient } from '@supabase/supabase-js';
 
 import { Database } from './database.types';
 
+// Declaración de tipos para import.meta.env
+declare global {
+  interface ImportMetaEnv {
+    readonly VITE_SUPABASE_URL: string;
+    readonly VITE_SUPABASE_KEY: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+}
+
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_KEY);
